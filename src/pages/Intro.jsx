22f532dfa8a1d0340d7684e92673714d83ff5201
yuniverse96 <easy9607@gmail.com>
2025-07-16 11:10:03 +0900

    #인트로 이미지 경로수정

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -4,13 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import '../style/intro.css';
 
 const icons = [
-    "/images/sun.png",
-    "/images/cloudy.png",
-    "/images/snow.png",
-    "/images/thunder.png",
-    "/images/sun_cloudy.png",
-    "/images/rain.png"
+    process.env.PUBLIC_URL + "/images/sun.png",
+    process.env.PUBLIC_URL + "/images/cloudy.png",
+    process.env.PUBLIC_URL + "/images/snow.png",
+    process.env.PUBLIC_URL + "/images/thunder.png",
+    process.env.PUBLIC_URL + "/images/sun_cloudy.png",
+    process.env.PUBLIC_URL + "/images/rain.png"
   ];
+  
   
 
 function Intro() {
@@ -22,37 +23,42 @@ function Intro() {
     const interval = setInterval(() => {
       gsap.to(imgRef.current, {
         duration: 0.5,
-        rotateY: 90,
+        rotateX: 90,
         opacity: 0,
         ease: "power1.in",
         onComplete: () => {
           setCurrentIdx((prev) => (prev + 1) % icons.length);
-
-          gsap.fromTo(imgRef.current,
-            { rotateY: -90, opacity: 0 },
+  
+          gsap.fromTo(
+            imgRef.current,
+            { rotateX: -90, opacity: 0 },
             {
               duration: 0.5,
-              rotateY: 0,
+              rotateX: 0,
               opacity: 1,
               ease: "power1.out"
             }
           );
         }
       });
-    }, 2000); // 2초마다 전환
-
+    }, 2000);
+  
     return () => clearInterval(interval);
   }, []);
+  
 
   return (
     <div id="intro">
       <div className='img_animation'>
-        <img
-          ref={imgRef}
-          className="weather-icon"
-          src={icons[currentIdx]}
-          alt="weather"
-        />
+          <div className="img_wrap">
+                <img
+                ref={imgRef}
+                className="weather-icon"
+                src={icons[currentIdx]}
+                alt="weather"
+                />
+          </div>
+       
       </div>
 
       <div className='txt_box'>
