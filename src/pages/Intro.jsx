75e352fafab1b16955ef7abab734ee36e5ec2761
yuniverse96 <easy9607@gmail.com>
2025-07-16 11:01:59 +0900

    #intro 작업

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,25 +1,70 @@
-import '../style/intro.css';
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
 import { useNavigate } from 'react-router-dom';
+import '../style/intro.css';
 
-
+const icons = [
+    "/images/sun.png",
+    "/images/cloudy.png",
+    "/images/snow.png",
+    "/images/thunder.png",
+    "/images/sun_cloudy.png",
+    "/images/rain.png"
+  ];
+  
 
 function Intro() {
+  const navigate = useNavigate(); 
+  const imgRef = useRef(null);
+  const [currentIdx, setCurrentIdx] = useState(0);
 
-    const navigate = useNavigate(); 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      gsap.to(imgRef.current, {
+        duration: 0.5,
+        rotateY: 90,
+        opacity: 0,
+        ease: "power1.in",
+        onComplete: () => {
+          setCurrentIdx((prev) => (prev + 1) % icons.length);
 
-    return (
-      <div id="intro">
-          <div className='txt_box'> 
-              <h2>오늘 어디가?</h2>
-              <p>where are you going?</p>
-          </div>
-          <div className='btn_wrap'>
-             <button onClick={() => navigate('/Home')}>시작하기</button>
-          </div>
-       
+          gsap.fromTo(imgRef.current,
+            { rotateY: -90, opacity: 0 },
+            {
+              duration: 0.5,
+              rotateY: 0,
+              opacity: 1,
+              ease: "power1.out"
+            }
+          );
+        }
+      });
+    }, 2000); // 2초마다 전환
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div id="intro">
+      <div className='img_animation'>
+        <img
+          ref={imgRef}
+          className="weather-icon"
+          src={icons[currentIdx]}
+          alt="weather"
+        />
       </div>
-    );
-  }
-  
-  export default Intro;
-  
+
+      <div className='txt_box'>
+        <h2>오늘 어디가?</h2>
+        <p>where are you going?</p>
+      </div>
+
+      <div className='btn_wrap'>
+        <button onClick={() => navigate('/Home')}>시작하기</button>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
